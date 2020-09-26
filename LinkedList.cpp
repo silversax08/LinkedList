6d44247878dacc4f6fda63f74f0903f3b66885db
Silversax08 <garrett.hawkins@icloud.com>
2020-09-25 20:13:39 -0600

    Initial file set up and creation

---
+++ LinkedList.cpp
@@ -0,0 +1,32 @@
+#include "myLibrary.hpp"
+
+
+void add_item_to_beginning_of_list(int *head, int value)
+{
+
+}
+
+void add_item_to_end_of_list(int *head, int value)
+{
+
+}
+
+void remove_first_item_in_list(int *head)
+{
+
+}
+
+void remove_last_item_in_list(int *head)
+{
+
+}
+
+void remove_all_instances_of_item_from_list(int *head, int value)
+{
+
+}
+
+void clear_linked_list(int *head)
+{
+
+}
